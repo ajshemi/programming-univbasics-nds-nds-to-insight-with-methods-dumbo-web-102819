78f2fb3942b4e6b3461352cc331daeb84081f83b
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-24 11:06:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,14 +25,10 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-
-    sum=0
-    obj[:movies].map do |value|
-      value[:worldwide_gross]
-      sum+=value[:worldwide_gross]
-    end
-    sum
-    #result[obj[:name]]=sum
+  sum=0
+  director_data[:movies].map do |value|
+    value[:worldwide_gross]
+    sum+=value[:worldwide_gross]
   end
-  
+  sum
 end
