ffa4f0320c9eb6c5396cd8ed87322b9363e4cf88
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-24 10:59:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,34 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
-  nil
+  result={}
+  nds.map do |obj|
+    obj[:name]
+    #obj[:movies]
+    sum=0
+    obj[:movies].map do |value|
+      value[:worldwide_gross]
+      sum+=value[:worldwide_gross]
+    end
+    result[obj[:name]]=sum
+  end
+
+  result
+  #nil
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  nds.map do |obj|
+    obj[:name]
+    sum=0
+    obj[:movies].map do |value|
+      value[:worldwide_gross]
+      sum+=value[:worldwide_gross]
+    end
+    sum
+    #result[obj[:name]]=sum
+  end
+  
 end
